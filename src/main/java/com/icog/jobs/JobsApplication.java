232709017c77f38f038dea0f2a19aba04e2350e6
Samuel M. Debela <113627346+skdebela@users.noreply.github.com>
2024-11-11 16:16:50 +0300

    initial commit

---
+++ src/main/java/com/icog/jobs/JobsApplication.java
@@ -0,0 +1,13 @@
+package com.icog.jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsApplication.class, args);
+	}
+
+}
