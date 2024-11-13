8879b870304dc50cf506e01e790544093f918215
Samuel M. Debela <113627346+skdebela@users.noreply.github.com>
2024-11-13 19:54:57 +0300

    added dirties context

--- src/test/java/com/icog/jobs/job/JobRepositoryTests.java
+++ src/test/java/com/icog/jobs/job/JobRepositoryTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -18,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class JobRepositoryTests {
     private JobRepository underTest;
 
