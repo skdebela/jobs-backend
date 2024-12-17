54d68a89a03cccf65a1a5428484472dc59205c77
Samuel M. Debela <113627346+skdebela@users.noreply.github.com>
2024-12-17 17:36:56 +0300

    Add environment variables config

--- src/main/java/com/icog/jobs/JobsApplication.java
+++ src/main/java/com/icog/jobs/JobsApplication.java
@@ -1,5 +1,6 @@
 package com.icog.jobs;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobsApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(dotenvEntry ->
+				System.setProperty(dotenvEntry.getKey(), dotenvEntry.getValue())
+		);
+
 		SpringApplication.run(JobsApplication.class, args);
 	}
 
