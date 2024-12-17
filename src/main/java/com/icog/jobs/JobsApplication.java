329ea46dbdc2e158db7f204df417b7742f93e474
Samuel M. Debela <113627346+skdebela@users.noreply.github.com>
2024-12-17 20:51:48 +0300

    Switched to spring-dotenv for environemnt control for production

--- src/main/java/com/icog/jobs/JobsApplication.java
+++ src/main/java/com/icog/jobs/JobsApplication.java
@@ -1,19 +1,13 @@
 package com.icog.jobs;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JobsApplication {
 
-	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(dotenvEntry ->
-				System.setProperty(dotenvEntry.getKey(), dotenvEntry.getValue())
-		);
-
-		SpringApplication.run(JobsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JobsApplication.class, args);
+    }
 
 }
